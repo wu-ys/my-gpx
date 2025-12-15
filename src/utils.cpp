5bfdb3da84070eacb80d4c3a4530558d6d720b44
wu-ys <wu-ys20@mails.tsinghua.edu.cn>
2025-12-15 19:11:47 +0800

    gcj02 and wgs84 conversion

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,6 +1,7 @@
 #include "utils.hpp"
 #include <cmath>
-
+#include <fstream>
+#include <sstream>
 namespace mygpx {
 
 long long to_timestamp_seconds(const std::string& iso8601) {
@@ -39,10 +40,68 @@ std::string from_timestamp_millis(long long timestamp) {
 double kmph_to_mps(double kmph) { return kmph / 3.6; }
 double mps_to_kmph(double mps) { return mps * 3.6; }
 
+bool out_of_china(double lng, double lat) {
+    return not (73.66 < lng && lng < 135.05 && 3.86 < lat && lat < 53.55);
+}
+
+double transformlat(double lng, double lat) {
+    double ret = -100.0 + 2.0 * lng + 3.0 * lat + 0.2 * lat * lat + 0.1 * lng * lat + 0.2 * sqrt(abs(lng));
+    ret += (20.0 * sin(6.0 * lng * PI) + 20.0 * sin(2.0 * lng * PI)) * 2.0 / 3.0;
+    ret += (20.0 * sin(lat * PI) + 40.0 * sin(lat / 3.0 * PI)) * 2.0 / 3.0;
+    ret += (160.0 * sin(lat / 12.0 * PI) + 320 * sin(lat * PI / 30.0)) * 2.0 / 3.0;
+    return ret;
+}
+
+double transformlng(double lng, double lat) {
+    double ret = 300.0 + lng + 2.0 * lat + 0.1 * lng * lng + 0.1 * lng * lat + 0.1 * sqrt(abs(lng));
+    ret += (20.0 * sin(6.0 * lng * PI) + 20.0 * sin(2.0 * lng * PI)) * 2.0 / 3.0;
+    ret += (20.0 * sin(lng * PI) + 40.0 * sin(lng / 3.0 * PI)) * 2.0 / 3.0;
+    ret += (150.0 * sin(lng / 12.0 * PI) + 300.0 * sin(lng / 30.0 * PI)) * 2.0 / 3.0;
+    return ret;
+}
+
+
+std::pair<double, double> gcj02_to_wgs84(double lat, double lon) {
+    double a = EARTH_EQUATOR_RADIUS;
+    double ee = EARTH_ECCENTRICITY;
+    if (out_of_china(lon, lat))
+        return std::make_pair(lon, lat);
+    else {
+        double dlat = transformlat(lon - 105.0, lat - 35.0);
+        double dlon = transformlng(lon - 105.0, lat - 35.0);
+        double radlat = lat / 180.0 * PI;
+        double magic = sin(radlat);
+        magic = 1 - ee * magic * magic;
+        double sqrtmagic = sqrt(magic);
+        dlat = (dlat * 180.0) / ((a * (1 - ee)) / (magic * sqrtmagic) * PI);
+        dlon = (dlon * 180.0) / (a / sqrtmagic * cos(radlat) * PI);
+        double mglat = lat + dlat;
+        double mglon = lon + dlon;
+        return std::make_pair(lon * 2 - mglon, lat * 2 - mglat);
+    }
+}
+
+std::pair<double, double> wgs84_to_gcj02(double lat, double lon) {
+    double a = EARTH_EQUATOR_RADIUS;
+    double ee = EARTH_ECCENTRICITY;
+    if (out_of_china(lon, lat))
+        return std::make_pair(lon, lat);
+    double dlat = transformlat(lon - 105.0, lat - 35.0);
+    double dlon = transformlng(lon - 105.0, lat - 35.0);
+    double radlat = lat / 180.0 * PI;
+    double magic = sin(radlat);
+    magic = 1 - ee * magic * magic;
+    double sqrtmagic = sqrt(magic);
+    dlat = (dlat * 180.0) / ((a * (1 - ee)) / (magic * sqrtmagic) * PI);
+    dlon = (dlon * 180.0) / (a / sqrtmagic * cos(radlat) * PI);
+    double mglat = lat + dlat;
+    double mglon = lon + dlon;
+    return std::make_pair(mglon, mglat);
+}
+
 double ComputePointDistance(double lat1, double lon1, double lat2, double lon2) {
 
-    const double pi = 3.14159265358979323846;
-    const double R = 6371000.0; // Earth radius in meters
+    const double R = EARTH_AVERAGE_RADIUS; // Earth radius in meters
     double lat1_rad = lat1 * PI / 180.0;
     double lon1_rad = lon1 * PI / 180.0;
     double lat2_rad = lat2 * PI / 180.0;
@@ -57,6 +116,21 @@ double ComputePointDistance(double lat1, double lon1, double lat2, double lon2) 
     double c = 2 * atan2(sqrt(a), sqrt(1 - a));
 
     return R * c; // Distance in meters
+
+}
+
+std::string getAmapAPIKey() {
+
+    std::string path = std::string(MY_SOURCE_DIR) + "/AMAP_API_KEY";
+
+    std::ifstream infile(path);
+    if (!infile.is_open()) {
+        throw std::runtime_error("Failed to open AMAP_API_KEY file at: " + path);
+    }
+    std::string apiKey;
+    std::getline(infile, apiKey);
+    infile.close();
+    return apiKey;
 
 }
 
--- src/utils.hpp
+++ src/utils.hpp
@@ -4,6 +4,10 @@
 namespace mygpx {
 
 constexpr double PI = 3.14159265358979323846;
+constexpr double EARTH_EQUATOR_RADIUS = 6378245.0;
+constexpr double EARTH_POLAR_RADIUS = 6356752.3142;
+constexpr double EARTH_AVERAGE_RADIUS = 6371000.0;
+constexpr double EARTH_ECCENTRICITY = 0.00669342162296594323;
 
 long long to_timestamp_seconds(const std::string& iso8601);
 long long to_timestamp_millis(const std::string& iso8601);
@@ -14,7 +18,12 @@ std::string from_timestamp_millis(long long timestamp);
 double kmph_to_mps(double kmph);
 double mps_to_kmph(double mps);
 
+std::pair<double, double> gcj02_to_wgs84(double lat, double lon);
+std::pair<double, double> wgs84_to_gcj02(double lat, double lon);
+
 double ComputePointDistance(double lat1, double lon1, double lat2, double lon2);
+
+std::string getAmapAPIKey();
 
 
 }
